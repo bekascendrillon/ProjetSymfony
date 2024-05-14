2cf4a82a6c1db024aacdd77c467f6a341a866ae5
BEKAS CENDRILLON <93581161+bekascendrillon@users.noreply.github.com>
2024-05-14 14:15:31 +0200

    Les controleurs sur la table administrateur et utilisateurs

--- src/Controller/AdministrateurController.php
+++ src/Controller/AdministrateurController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Administrateur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdministrateurController extends AbstractController
 {
     #[Route('/administrateur', name: 'app_administrateur')]
     public function index(): Response
     {
-        return $this->render('administrateur/index.html.twig', [
-            'controller_name' => 'AdministrateurController',
-        ]);
+        $administrateur = new Administrateur();
+        $administrateur->setNom('Blaise');
+        $administrateur->setPrenom('Pascal');
+        $administrateur->setPseudo('KingBlaiso');
+        $administrateur->setMdp('Bp_king.22');
+        $administrateur->setNomRole('Supprimer Compte');
+
+
+        return new Response('Saved new administrateur with id '.$administrateur->getId());
     }
 }
--- src/Controller/UtilisateursController.php
+++ src/Controller/UtilisateursController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateurs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UtilisateursController extends AbstractController
 {
     #[Route('/utilisateurs', name: 'app_utilisateurs')]
     public function index(): Response
     {
-        return $this->render('utilisateurs/index.html.twig', [
-            'controller_name' => 'UtilisateursController',
-        ]);
+
+        $utilisateur = new Utilisateurs();
+        $utilisateur->setPseudo('Flore');
+        $utilisateur->setEmail('flora@gmail.com');
+        $utilisateur->setMdp('Me_andyou.22');
+
+
+        return new Response('Saved new utilisateur with id '.$utilisateur->getId());
+
     }
+
 }
+
+
